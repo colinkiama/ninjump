a8ea8a034506c556dc087448c149db2cb44442c7
Colin Kiama <colinkiama@gmail.com>
2024-03-06 14:31:06 +0000

    Game is now always scales to available space while keeping logical width of 300

--- src/config.ts
+++ src/config.ts
@@ -1,14 +1,16 @@
 import Phaser from "phaser";
 
+let screenRatio = window.innerHeight / window.innerWidth;
+
 export default {
   type: Phaser.AUTO,
   parent: "game",
   backgroundColor: "#33A5E7",
   scale: {
     width: 300,
-    height: 400,
+    height: 300 * screenRatio,
     mode: Phaser.Scale.FIT,
-    autoCenter: Phaser.Scale.CENTER_BOTH,
+    autoCenter: Phaser.Scale.CENTER_HORIZONTALLY,
   },
   physics: {
     default: "arcade",
--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,24 @@ import GameScene from "./scenes/Game";
 import GameOver from "./scenes/GameOver";
 import Score from "./scenes/Score";
 
-new Phaser.Game(
+const game = new Phaser.Game(
   Object.assign(config, {
     scene: [GameScene, Score, GameOver],
   })
 );
+
+function resize() {
+  game.scale.setGameSize(
+    game.scale.width,
+    game.scale.width * calculateScreenRatio()
+  );
+
+  game.scale.setMaxZoom();
+}
+
+function calculateScreenRatio() {
+  return window.innerHeight / window.innerWidth;
+}
+
+game.scale.setMaxZoom();
+window.addEventListener("resize", resize);
